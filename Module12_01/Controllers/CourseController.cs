9f7cc01b51a817e75ae578ed42fc1f49574bdfcc
auxo86 <auxo86@gmail.com>
2017-06-06 20:24:36 +0800

    練習WebAPI

---
+++ Module12_01/Controllers/CourseController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Module12_01.Models;
+
+namespace Module12_01.Controllers
+{
+    public class CourseController : ApiController
+    {
+        //Test Data
+        private static List<Course> Courses = new List<Course>
+        {
+            new Course {Id=1, Name="MVC"},
+            new Course {Id=2, Name="WebForm"},
+            new Course {Id=3, Name="WebAPI"}
+        };
+
+        // GET: api/Course
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Course/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Course
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Course/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Course/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
